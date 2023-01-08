842f7213180a09bdac10f3185b6814b1aa0620ef
lxwulf <44492381+lxwulf@users.noreply.github.com>
2023-01-08 18:22:33 +0100

    ⚙🪛 Reworked variables from the download function

--- distro_dl/download.py
+++ distro_dl/download.py
@@ -6,9 +6,6 @@ import requests
 from bs4 import BeautifulSoup as BS4_Crawler
 from tqdm import tqdm
 
-STATUS = 200
-FILETYPE = "qcow2"
-
 
 def get_link(source):
     return BS4_Crawler(requests.get(source).text, "html.parser")
@@ -16,16 +13,22 @@ def get_link(source):
 
 def image(name, version):
 
-    main_source = f"http://ftp.usf.edu/pub/fedora/linux/releases/{version}/" \
-                  f"Cloud/x86_64/images/"
+    status = 200
+
+    if name == "fedora":
+        main_source = f"http://ftp.usf.edu/pub/fedora/linux/releases/{version}/" \
+                      f"Cloud/x86_64/images/"
+        filetype = "qcow2"
+    elif name == "ubuntu":
+        main_source = f"https://cloud-images.ubuntu.com/{version}/current/"
+        filetype = "kvm.img"
 
     for LINKS in get_link(main_source).findAll('a'):
         image_link = LINKS.get('href')
 
         get_file = requests.get(main_source + image_link, stream=True)
 
-        if STATUS == get_file.status_code and FILETYPE in image_link:
-
+        if status == get_file.status_code and filetype in image_link:
             with open(LINKS.text, 'wb') as TARGET_FILE, tqdm(
                     desc=image_link,
                     total=int(get_file.headers.get("Content-Length", "0")),
@@ -34,7 +37,6 @@ def image(name, version):
                     unit_scale=True,
                     unit_divisor=1024
             ) as progressbar:
-
                 for DATA in get_file.iter_content(chunk_size=1024):
                     size = TARGET_FILE.write(DATA)
                     progressbar.update(size)
