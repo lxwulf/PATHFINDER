c7022bff1b7407c93d782df9f340e5a7ea27a6ee
lxwulf <44492381+lxwulf@users.noreply.github.com>
2023-01-08 18:12:56 +0100

    ⚙️🪛 Reworked the launch query

--- pathfinder.py
+++ pathfinder.py
@@ -10,9 +10,8 @@ import subprocess
 import click
 
 import distro_dl
-from distro_dl import fedora
+from distro_dl import download
 from distro_dl import file
-from distro_dl import ubuntu
 
 subprocess.run("clear")
 
@@ -20,22 +19,15 @@ subprocess.run("clear")
 @click.command(no_args_is_help=True)
 @click.argument("name", type=str)
 @click.argument("version", type=str)
-@click.option("--counter", default=1, type=int,
-              help="You can set the number how many images you want to "
-                   "download.")
-def main(name, version, counter):
-    if name == "fedora" and counter == 1:
-        distro_dl.fedora.download(version)
-    elif name == "fedora" and counter != 1:
-        for number in range(1, counter + 1):
-            distro_dl.fedora.download(version)
-            distro_dl.file.rename(number, name)
-    elif name == "ubuntu" and counter == 1:
-        distro_dl.ubuntu.download(version)
-    elif name == "ubuntu" and counter != 1:
-        for number in range(1, counter + 1):
-            distro_dl.ubuntu.download(version)
-            distro_dl.file.rename(number, name)
+@click.option("--count", default=1, type=int, show_default=True,
+              help="You can set the number how many images you want to download.")
+def main(name, version, count):
+    if count != 1:
+        for number in range(1, count+1):
+            distro_dl.download.image(name, version)
+            distro_dl.file.rename(name, number)
+    else:
+        distro_dl.download.image(name, version)
 
 
 if __name__ == "__main__":
